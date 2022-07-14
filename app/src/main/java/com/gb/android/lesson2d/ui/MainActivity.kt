f6f07c0e048346585a049ab54f9a618002207418
mafusail <mikelnord@gmail.com>
2022-07-14 20:56:38 +1000

    add splashScreen

--- app/src/main/java/com/gb/android/lesson2d/ui/MainActivity.kt
+++ app/src/main/java/com/gb/android/lesson2d/ui/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.gb.android.lesson2d.ui
 
+import android.animation.ObjectAnimator
+import android.os.Build
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import android.view.ViewTreeObserver
+import android.view.animation.AnticipateInterpolator
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.doOnEnd
 import com.gb.android.lesson2d.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,5 +17,40 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            var isHideSplashScreen = false
+            object : CountDownTimer(2000, 1000) {
+                override fun onTick(millisUntilFinished: Long) {}
+                override fun onFinish() {
+                    isHideSplashScreen = true
+                }
+            }.start()
+            val content: View = findViewById(android.R.id.content)
+            content.viewTreeObserver.addOnPreDrawListener(
+                object : ViewTreeObserver.OnPreDrawListener {
+                    override fun onPreDraw(): Boolean {
+                        return if (isHideSplashScreen) {
+                            content.viewTreeObserver.removeOnPreDrawListener(this)
+                            true
+                        } else {
+                            false
+                        }
+                    }
+                }
+            )
+            splashScreen.setOnExitAnimationListener { splashScreenView ->
+                val slideUp = ObjectAnimator.ofFloat(
+                    splashScreenView,
+                    View.TRANSLATION_Y,
+                    0f,
+                    -splashScreenView.height.toFloat()
+                )
+                slideUp.interpolator = AnticipateInterpolator()
+                slideUp.duration = 200L
+                slideUp.doOnEnd { splashScreenView.remove() }
+                slideUp.start()
+            }
+        }
     }
 }
